52b324267a64cd31086841e1bfbe837b68366a4e
Kuldeep Parihar <kldpsingh916@gmail.com>
2026-01-18 21:06:41 +0530

    added search bar

--- assignment-2/easy/The-Pokémon/script.js
+++ assignment-2/easy/The-Pokémon/script.js
@@ -6,11 +6,13 @@ function spawnPokemons(number) {
         .then(response => response.json())
         .then(data => {
             let item = {
+                id: data.id,
                 name: data.name,
                 photo: data.sprites.front_default,
                 type: data.types[0].type.name
             }
             createCards(item);
+            pokemonList.push(item);
         })
         .catch(error => console.error('Error:', error));
 }
@@ -20,17 +22,41 @@ for (let i = 1; i <= pokeLimit; i++) {
 
 function createCards(item) {
     const container = document.getElementById("container");
-    let cardHtml = `
-    <div class="card">
-        <div class="card-title" id="pokeName" style="text-transform: capitalize;">${item.name}</div>
-        <div class="card-img">
-        <img id="pokePhoto" src="${item.photo}" alt="Pokemon Image" width="200" height="200">
-        </div>
-        <div class="pill ${item.type}">${item.type}</div>
-    </div>`
-
+    const cardHtml = getCard(item);
     var card = document.createElement('div');
     card.classList.add('rows');
     card.innerHTML = cardHtml;
     container.appendChild(card);
+}
+
+function getCard(item){
+    return `
+        <div class="card">
+            <div class="card-title" id="pokeName" style="text-transform: capitalize;">${item.name}</div>
+            <div class="card-img">
+            <img id="pokePhoto" src="${item.photo}" alt="Pokemon Image" width="200" height="200">
+            </div>
+            <div class="pill ${item.type}">${item.type}</div>
+        </div>`
+}
+
+function findPokemon(){
+    const filterHtml = document.getElementById("filtered");
+    filterHtml.replaceChildren();
+
+    var input = document.getElementById('myInput');
+	var search = input.value.toUpperCase();
+    if(!search){
+        document.getElementById("container").style.setProperty('display', 'flex');
+        return;
+    }
+
+    document.getElementById("container").style.setProperty('display', 'none');
+    const filterList = pokemonList.filter(x=> x.name.toUpperCase().includes(search) || x.id == search);
+    filterList.forEach(pokemon => {
+        console.log(pokemon)
+        var filterCard = document.createElement('div');
+        filterCard.innerHTML = getCard(pokemon);
+        filterHtml.appendChild(filterCard);
+    });
 }
