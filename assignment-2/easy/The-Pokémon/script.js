7bb6f56068396f01caef17b4fa15c22b9c22f510
Kuldeep Parihar <kldpsingh916@gmail.com>
2026-01-18 22:36:53 +0530

    added async function

--- assignment-2/easy/The-Pokémon/script.js
+++ assignment-2/easy/The-Pokémon/script.js
@@ -1,60 +1,56 @@
-let pokemonList = []
+const pokemonList = [];
 const pokeLimit = 151;
+
 for (let i = 1; i <= pokeLimit; i++) {
     spawnPokemons(i);
 }
 
-function spawnPokemons(number, isNew = false) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
-        .then(response => response.json())
-        .then(data => {
-            let item = {
-                id: data.id,
-                name: data.name,
-                photo: data.sprites.front_default,
-                type: data.types[0].type.name
-            }
-            if (isNew)
-                addFilteredCard(item);
-            
-            createCards(item);
-            pokemonList.push(item);
-        })
-        .catch(error => console.error('Error:', error));
+async function spawnPokemons(number, isNew = false) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+        const data = await response.json();
+        const item = {
+            id: data.id,
+            name: data.name,
+            photo: data.sprites.front_default,
+            type: data.types[0].type.name
+        };
+        createCards(item, isNew);
+        pokemonList.push(item);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 function getCard(item) {
     return `
-        <div class="card">
-            <div class="card-title" id="pokeName" style="text-transform: capitalize;">${item.name}</div>
-            <div class="card-img">
-                <img id="pokePhoto" src="${item.photo}" alt="Pokemon Image" width="200" height="200">
-            </div>
-            <div class="pill ${item.type}">${item.type}</div>
-        </div>`
+            <div class="card">
+                <div class="card-title" id="pokeName" style="text-transform: capitalize;">${item.name}</div>
+                <div class="card-img">
+                    <img id="pokePhoto" src="${item.photo}" alt="Pokemon Image" width="200" height="200">
+                </div>
+                <div class="pill ${item.type}">${item.type}</div>
+            </div>`;
 }
 
-function createCards(item) {
+function createCards(item, isNew = false) {
     const cardHtml = getCard(item);
-
-    var card = document.createElement('div');
+    const card = document.createElement('div');
     card.innerHTML = cardHtml;
-
-    const container = document.getElementById('container');
-    container.appendChild(card);
+    const targetContainer = isNew ? document.getElementById("filtered") : document.getElementById('container');
+    targetContainer.appendChild(card);
 }
 
 function showMain(show) {
     const main = document.getElementById("container").style;
-    main.setProperty('display', show ? 'flex' : 'none');
-
+    main.display = show ? 'flex' : 'none';
     const filterHtml = document.getElementById("filtered").style;
-    filterHtml.setProperty('display', !show ? 'flex' : 'none');
+    filterHtml.display = !show ? 'flex' : 'none';
 }
 
 function findPokemon() {
-    var input = document.getElementById('myInput');
-    var search = input.value.toUpperCase();
+    const input = document.getElementById('myInput');
+    const search = input.value.toUpperCase();
     if (!search) {
         showMain(true);
         return;
@@ -64,15 +60,13 @@ function findPokemon() {
     const filterList = pokemonList.filter(x => x.name.toUpperCase().includes(search) || x.id == search);
     filterList.forEach(pokemon => addFilteredCard(pokemon));
 
-    if (Number.isInteger(Number(search)) && filterList.length == 0)
+    if (Number.isInteger(Number(search)) && filterList.length === 0) {
         spawnPokemons(search, true);
+    }
 }
 
 function addFilteredCard(pokemon) {
     const filterHtml = document.getElementById("filtered");
-    filterHtml.replaceChildren();
-
-    var filterCard = document.createElement('div');
-    filterCard.innerHTML = getCard(pokemon);
-    filterHtml.appendChild(filterCard);
+    filterHtml.replaceChildren(); // Clear previous cards
+    createCards(pokemon, true);
 }
